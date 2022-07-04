c39f3d3d04d827d74b23d86831ba407f9367445e
abhyudaysoni <abhyudaysoni03@gmail.com>
2022-07-04 18:55:02 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+//selecting all the rating buttons
+const ratings = document.querySelectorAll(".rating");
+
+//selecting the submit button
+const sub = document.querySelector(".submit-btn");
+
+//selecting rating-page
+const ratingPage = document.querySelector(".rating-page");
+
+//selecting thank-you-page
+const ThankYouPage = document.querySelector(".thank-you-page");
+
+//saving the rated button
+let ratedButton;
+
+//after pressing the submit button
+function submitFeedback() {
+    //removing the rating page
+    ratingPage.style.display = "none";
+
+    //changing the ratings
+    const selectedRating = document.querySelector(".selected-rating");
+
+    selectedRating.innerHTML = `You selected ${ratedButton} out of 5`;
+
+    //displaying the thank you page
+    ThankYouPage.style.display = "grid";
+}
+
+ratings.forEach((element) => {
+    element.addEventListener("click", function () {
+        ratings.forEach((element2) => {
+            element2.classList.remove("active");
+
+            element2.style.backgroundColor = " #2b3240";
+        });
+
+        element.classList.add("active");
+
+        element.style.backgroundColor = " #fb7413";
+
+        ratedButton = element.value;
+
+        sub.addEventListener("click", submitFeedback);
+    });
+});
